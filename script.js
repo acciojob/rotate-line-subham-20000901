d871fd321c6299dfdba43ec8b6f5e671cb6bcb15
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-19 17:43:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let line = document.getElementById("line");
+let angle = 0;
+ setInterval(function(){
+	angle += 2;
+	line.style.transform= `translate(-50%,-50%) rotate(${angle}deg)`;
+},20);
